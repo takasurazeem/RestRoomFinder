fa8fe553663e4d34865f515f2df586eb248b3654
Takasur Azeem <takasurazeem@gmail.com>
2024-06-09 08:54:17 +0500

    Initial Commit

---
+++ RestRoomFinder/RestRoomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestRoomFinderApp.swift
+//  RestRoomFinder
+//
+//  Created by Takasur Home on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestRoomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
